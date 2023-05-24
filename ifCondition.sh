0cdd3adb3bda2e58571e70d010951c16c121b2db
vishalbandhekar <vishalbandhekar@gmail.com>
2023-05-24 13:31:46 +0530

    if condition

---
+++ ifCondition.sh
@@ -0,0 +1,13 @@
+echo "PLease enter the number"
+read x
+
+if  test  $x -eq 10
+then
+  echo "Equal to 10"
+elif  test  $x  -gt  10
+then
+  echo "Greater than 10"
+else
+  echo "Less than 10"
+fi
+
